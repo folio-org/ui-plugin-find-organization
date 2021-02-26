bb0914dd0a167cd91769f1ed90dfd18fc5b08564
AndreiShumski2 <Andrei_Shumski2@epam.com>
2021-02-26 16:33:10 +0300

    FAT-42 Tests (#75)
    * FAT-42 Tests
    
    * f

--- OrganizationSearch/OrganizationsListContainer.test.js
+++ OrganizationSearch/OrganizationsListContainer.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render, act, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
 
 import OrganizationsListContainer from './OrganizationsListContainer';
 
 jest.mock('./OrganizationsList', () => {
-  return () => <span>OrganizationsList</span>;
+  // eslint-disable-next-line react/prop-types
+  return ({ onNeedMoreData, applyFilters }) => (
+    <>
+      <button type="button" onClick={onNeedMoreData}>OnNeedMoreData</button>;
+      <button type="button" onClick={() => applyFilters('status', ['Active'])}>applyFilters</button>;
+    </>
+  );
 });
 
 const renderOrganizationsListContainer = (mutator) => (render(
@@ -15,17 +22,41 @@ const renderOrganizationsListContainer = (mutator) => (render(
 ));
 
 describe('OrganizationsListContainer component', () => {
-  it('should not fetch organizations when plugin is open by default ', async () => {
-    const mutator = {
+  let mutator;
+
+  beforeEach(() => {
+    mutator = {
       organizationsListOrgs: {
-        GET: jest.fn(),
+        GET: jest.fn(() => Promise.resolve({ organizations: [], totalRecords: 0 })),
       },
     };
+  });
 
+  it('should not fetch organizations when plugin is open by default', async () => {
     await act(async () => {
       renderOrganizationsListContainer(mutator);
     });
 
     expect(mutator.organizationsListOrgs.GET).not.toHaveBeenCalled();
+  });
+
+  it('should not fetch organizations since filters were not applied', async () => {
+    renderOrganizationsListContainer(mutator);
+
+    await waitFor(() => {
+      user.click(screen.getByText('OnNeedMoreData'));
+    });
+
+    expect(mutator.organizationsListOrgs.GET).not.toHaveBeenCalled();
+  });
+
+  it('should fetch organizations with applied Status filter', async () => {
+    renderOrganizationsListContainer(mutator);
+
+    await waitFor(() => {
+      user.click(screen.getByText('applyFilters'));
+    });
+
+    expect(mutator.organizationsListOrgs.GET).toHaveBeenCalled();
   });
 });
