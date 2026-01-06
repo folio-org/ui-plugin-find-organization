f86dbe09ebeb4f7624a44a3da2c905b05e8172e5
Yury Saukou <yury_saukou@epam.com>
2026-01-06 15:53:04 +0400

    UIPFO-61 Remove usage of the moment library

--- OrganizationSearch/hooks/useOrganizations/useOrganizations.test.js
+++ OrganizationSearch/hooks/useOrganizations/useOrganizations.test.js
@@ -7,7 +7,9 @@ import {
   SEARCH_PARAMETER,
   VENDORS_API,
 } from '@folio/stripes-acq-components';
+import { NO_DST_TIMEZONES } from '@folio/stripes-acq-components/test/jest/fixtures';
 
+import { FILTERS } from '../../constants';
 import { useOrganizations } from './useOrganizations';
 
 jest.mock('@folio/stripes/core', () => ({
@@ -26,23 +28,20 @@ const stripesStub = {
   hasPerm: jest.fn(() => true),
 };
 
+const renderTestHook = (...args) => renderHook(() => useOrganizations(...args));
+
 describe('useOrganizations', () => {
   beforeEach(() => {
-    getMock.mockClear();
-    stripesStub.hasPerm.mockClear();
+    useOkapiKy.mockReturnValue({ get: getMock });
+    useStripes.mockReturnValue(stripesStub);
+  });
 
-    useOkapiKy
-      .mockClear()
-      .mockReturnValue({
-        get: getMock,
-      });
-    useStripes
-      .mockClear()
-      .mockReturnValue(stripesStub);
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should not make a get a request to fetch orgs when fetchOrganizations is called and without filters', async () => {
-    const { result } = renderHook(() => useOrganizations());
+    const { result } = renderTestHook();
 
     await result.current.fetchOrganizations({ searchParams: {}, offset: 0, limit: 30 });
 
@@ -50,7 +49,7 @@ describe('useOrganizations', () => {
   });
 
   it('should make a get a request to fetch orgs when fetchOrganizations is called and with filters', async () => {
-    const { result } = renderHook(() => useOrganizations());
+    const { result } = renderTestHook();
 
     await result.current.fetchOrganizations({ searchParams: { status: 'Active' }, offset: 0, limit: 30 });
 
@@ -67,7 +66,7 @@ describe('useOrganizations', () => {
   });
 
   it('should return orgs when fetchOrganizations is called and with filters', async () => {
-    const { result } = renderHook(() => useOrganizations());
+    const { result } = renderTestHook();
 
     const { organizations } = await result.current.fetchOrganizations({ searchParams: { status: 'Active' }, offset: 0, limit: 30 });
 
@@ -82,7 +81,7 @@ describe('useOrganizations', () => {
     };
 
     it('should include banking information index in the query if a user has the appropriate permission', async () => {
-      const { result } = renderHook(() => useOrganizations());
+      const { result } = renderTestHook();
 
       await result.current.fetchOrganizations(params);
 
@@ -96,7 +95,7 @@ describe('useOrganizations', () => {
     it('should NOT include banking information index in the query if a user does not have the appropriate permission', async () => {
       stripesStub.hasPerm.mockReturnValue(false);
 
-      const { result } = renderHook(() => useOrganizations());
+      const { result } = renderTestHook();
 
       await result.current.fetchOrganizations(params);
 
@@ -105,6 +104,74 @@ describe('useOrganizations', () => {
           [SEARCH_PARAMETER]: expect.not.stringContaining('bankingInformation.bankAccountNumber="qwerty*"'),
         }),
       }));
+    });
+  });
+
+  describe('Datetime filters', () => {
+    const dateTimeConfig = {
+      from: '2014-07-14',
+      to: '2020-07-14',
+    };
+
+    const expectedResultsDict = {
+      [NO_DST_TIMEZONES.AFRICA_DAKAR]: {
+        start: '2014-07-14T00:00:00.000',
+        end: '2020-07-14T23:59:59.999',
+      },
+      [NO_DST_TIMEZONES.AMERICA_BOGOTA]: {
+        start: '2014-07-14T05:00:00.000',
+        end: '2020-07-15T04:59:59.999',
+      },
+      [NO_DST_TIMEZONES.ASIA_DUBAI]: {
+        start: '2014-07-13T20:00:00.000',
+        end: '2020-07-14T19:59:59.999',
+      },
+      [NO_DST_TIMEZONES.ASIA_SHANGHAI]: {
+        start: '2014-07-13T16:00:00.000',
+        end: '2020-07-14T15:59:59.999',
+      },
+      [NO_DST_TIMEZONES.ASIA_TOKIO]: {
+        start: '2014-07-13T15:00:00.000',
+        end: '2020-07-14T14:59:59.999',
+      },
+      [NO_DST_TIMEZONES.EUROPE_MOSCOW]: {
+        start: '2014-07-13T20:00:00.000',
+        end: '2020-07-14T20:59:59.999',
+      },
+      [NO_DST_TIMEZONES.PACIFIC_TAHITI]: {
+        start: '2014-07-14T10:00:00.000',
+        end: '2020-07-15T09:59:59.999',
+      },
+      [NO_DST_TIMEZONES.UTC]: {
+        start: '2014-07-14T00:00:00.000',
+        end: '2020-07-14T23:59:59.999',
+      },
+    };
+
+    const datetimeFilters = [
+      FILTERS.DATE_CREATED,
+      FILTERS.DATE_UPDATED,
+    ];
+
+    describe.each(datetimeFilters)('Filter: %s', (filter) => {
+      it.each(Object.keys(expectedResultsDict))('should properly apply filter for the timezone - %s', async (timezone) => {
+        useStripes.mockReturnValue({
+          ...stripesStub,
+          timezone,
+        });
+
+        const { start, end } = expectedResultsDict[timezone];
+
+        const { result } = renderTestHook();
+
+        await result.current.fetchOrganizations({
+          searchParams: {
+            [filter]: [dateTimeConfig.from, dateTimeConfig.to].join(':'),
+          },
+        });
+
+        expect(getMock.mock.calls[0][1].searchParams.query).toContain(`(${filter}>="${start}" and ${filter}<="${end}")`);
+      });
     });
   });
 });
